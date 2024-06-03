910d8697c7b3893423f08b9066365189ed4c7012
Kacper Mularczyk <163023643+noWayToFindNick@users.noreply.github.com>
2024-06-04 01:27:24 +0200

    Add files via upload

--- wypozyczalnia/init.php
+++ wypozyczalnia/init.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Framework initialization
+ * - load config, messages, autoloader, router - prepare functions returning this global objects
+ * - prepare functions loading smarty, twig, database and autoloader on demand (only once)
+ * - load core functions, user roles from session and load action name to routing
+ *
+ *  * @author Przemysław Kudłacik
+ */
 require_once 'core/Config.class.php';
 require_once 'core/App.class.php';
 
--- wypozyczalnia/routing.php
+++ wypozyczalnia/routing.php
@@ -13,5 +13,40 @@ Utils::addRoute('loginView', 'LogCtrl');
 Utils::addRoute('registerView', 'RegCtrl');
 Utils::addRoute('reservationView', 'ReservCtrl');
 
+Utils::addRoute('bikesOnly', 'ReservCtrl'); //AJAX
+Utils::addRoute('tableOnly', 'ForAdminCtrl'); //AJAX
+Utils::addRoute('rentalsOnly', 'ForWorkerCtrl'); //AJAX
+Utils::addRoute('rentalsOnlyUser', 'ForUserCtrl'); //AJAX
+Utils::addRoute('rentalsToAcceptOnly', 'ForWorkerCtrl'); //AJAX
+
+Utils::addRoute('addBikeView', 'ForWorkerCtrl');
+Utils::addRoute('deleteBikeView', 'ForWorkerCtrl');
+Utils::addRoute('editBikeView', 'ForWorkerCtrl');
+Utils::addRoute('deleteUserView', 'ForAdminCtrl');
+Utils::addRoute('addRoleToUserView', 'ForAdminCtrl');
+Utils::addRoute('disableRoleToUserView', 'ForAdminCtrl');
+Utils::addRoute('addRoleView', 'ForAdminCtrl');
+Utils::addRoute('disableRoleView', 'ForAdminCtrl');
+Utils::addRoute('acceptRentalsView', 'ForWorkerCtrl');
+Utils::addRoute('rentalsView', 'ForWorkerCtrl');
+Utils::addRoute('userRentalsView', 'ForUserCtrl');
+Utils::addRoute('adminPanelView', 'ForAdminCtrl');
+
 Utils::addRoute('login', 'LogCtrl');
 Utils::addRoute('logout', 'LogCtrl');
+Utils::addRoute('register', 'RegCtrl');
+Utils::addRoute('addBike', 'ForWorkerCtrl');
+Utils::addRoute('deleteBike', 'ForWorkerCtrl');
+Utils::addRoute('editBike', 'ForWorkerCtrl');
+Utils::addRoute('deleteUser', 'ForAdminCtrl');
+Utils::addRoute('addRoleToUser', 'ForAdminCtrl');
+Utils::addRoute('disableRoleToUser', 'ForAdminCtrl');
+Utils::addRoute('addRole', 'ForAdminCtrl');
+Utils::addRoute('disableRole', 'ForAdminCtrl');
+Utils::addRoute('reservation', 'ReservCtrl');
+
+Utils::addRoute('accept', 'ForWorkerCtrl');
+Utils::addRoute('decline', 'ForWorkerCtrl');
+
+Utils::addRoute('inTerm', 'ForWorkerCtrl');
+Utils::addRoute('notInTerm', 'ForWorkerCtrl');
